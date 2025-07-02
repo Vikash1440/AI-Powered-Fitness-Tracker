e1821f276097ed0a211b5ed2ab2ac1a1503e828d
Vikash1440 <meenavikash34262@gmail.com>
2025-07-02 17:50:53 +0530

    deployment

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -4,5 +4,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('api.urls'))
+    path('api/',include('api.urls')),
 ]
+
+urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
